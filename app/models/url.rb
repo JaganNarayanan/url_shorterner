1ffe732ba85249455aa347689fa6f357e12b9b5e
JaganNarayanan <jns405@nyu.edu>
2017-08-03 16:50:05 +0800

    added validations for Url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,10 @@
 class Url < ApplicationRecord
+  alidates :long, presence: true, uniqueness: true
+  validates :long, format: { with: URI.regexp }, if: 'long.present?'
+  before_create :shorten
+
+  def shorten
+    self.short = SecureRandom.hex(10)
+  end
+  
 end
